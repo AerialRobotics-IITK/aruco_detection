c698c860249d9de2ad47494caad29874c3b8993e
PratyushGupta0 <gpratyush20@iitk.ac.in>
2022-12-18 13:41:47 +0530

    Reduce resolution

--- aruco_detector/src/aruco_detector_node.cpp
+++ aruco_detector/src/aruco_detector_node.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
     ros::init(argc, argv, "aruco_detector_node");
     ros::NodeHandle nh;
-    ros::Rate loop_rate(30);
+    ros::Rate loop_rate(10);
     Aruco::ArucoDetector detector(nh, cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50), cv::aruco::DetectorParameters::create(), "/camera/image_raw");
     while (ros::ok()) {
         ros::spinOnce();
